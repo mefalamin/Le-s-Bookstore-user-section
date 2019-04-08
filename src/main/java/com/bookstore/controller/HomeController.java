5cadc3583e63a444d6777569e5c6472a768def42
Al-Amin Ebne Faiz <mefalamin@live.com>
2019-04-09 02:19:42 +0600

    first commit after index page add

---
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * created by saikat on 4/9/19
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+        return "index";
+    }
+}
