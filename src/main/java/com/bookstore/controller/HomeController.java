da71f07a8bb316658fbe47e021f1f8e4cbd85e2e
Al-Amin Ebne Faiz <mefalamin@live.com>
2019-04-10 02:13:39 +0600

    Adding my account page.closes #12

--- src/main/java/com/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -13,4 +13,9 @@ public class HomeController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping({"myAccount"})
+    public String myAccount(){
+        return "my-account";
+    }
 }
