43f40630ead99e9aae732e0aca32c9a2db3aab0a
Al-Amin Ebne Faiz <mefalamin@live.com>
2019-04-11 01:50:43 +0600

    SecurityService and configuration .closes #17 half

---
+++ src/main/java/com/bookstore/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.bookstore.repository;
+
+import com.bookstore.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * created by saikat on 4/11/19
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+}
